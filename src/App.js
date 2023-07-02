6a201e2fdd5cac95add5d3e4386370d6bebed616
juleni <69912179+juleni@users.noreply.github.com>
2023-07-02 22:40:59 +0200

    Added content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div className="App">Hello</div>;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Zvolen&appid=72285a45a8bcf47b8b7221e26f281fa0";
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">Porto</div>
+          <div className="temp">
+            <h1>25&deg;C</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">25&deg;C</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">25 km/h</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
